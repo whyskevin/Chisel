30dc2f6ed16689aea86cde09c04491c603a1dddf
whyskevin <ksyao@cpp.edu>
2018-11-26 17:09:22 -0800

    Completed dates can be graphed on scatterplot - xAxis labels are not populated

--- CreatingAHabit/app/src/main/java/com/example/creatingahabit/DescriptionActivity.java
+++ CreatingAHabit/app/src/main/java/com/example/creatingahabit/DescriptionActivity.java
@@ -26,11 +26,14 @@ import android.widget.Toast;
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.charts.ScatterChart;
 
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineDataSet;
 import com.github.mikephil.charting.data.ScatterData;
 import com.github.mikephil.charting.data.ScatterDataSet;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
 import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 import com.github.mikephil.charting.interfaces.datasets.IScatterDataSet;
 import com.prolificinteractive.materialcalendarview.CalendarDay;
@@ -42,9 +45,14 @@ import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
 
 import org.threeten.bp.DayOfWeek;
 
+import java.lang.reflect.Array;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Collection;
+import java.util.Date;
 
 import static android.Manifest.permission_group.CALENDAR;
 import static android.provider.AlarmClock.EXTRA_MESSAGE;
@@ -127,6 +135,24 @@ public class DescriptionActivity extends AppCompatActivity {
                 }
             }
         });
+
+        Cursor cursor = myDB.getAllDataHRSortedByDate(habitName);
+        if(cursor.getCount() == 0) {
+            Toast.makeText(this, "SORTED DATE EMPTY", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            while(cursor.moveToNext()) {
+                int complete = Integer.valueOf(cursor.getString(2));
+                String s = cursor.getString(1);
+                Log.d("I", "OHCOMEON: " + s);
+//                if(complete > 0) {
+//                    completedDays.add(new Entry(i ,1));
+//                }else{
+//                    completedDays.add(new Entry(i ,0));
+//                }
+//                i++;
+            }
+        }
 
 //        testMPAndroidChart();
         chart();
@@ -223,32 +249,95 @@ public class DescriptionActivity extends AppCompatActivity {
 
     //Plots (10,10) on the Scatter plot in the Description Activity
     public void testMPAndroidChart(){
-        //Testing Scatter Data: hard code point (10,10)
-        ArrayList<Entry> list1 = new ArrayList<Entry>();
-        //Populate the list with new "Entries". These will be data points of the
-        list1.add(new Entry(10,10));
-        ScatterDataSet dataSet = new ScatterDataSet(list1, "Kevin's Data");
-        dataSet.setAxisDependency(YAxis.AxisDependency.LEFT);
-        ArrayList<IScatterDataSet> dataSets = new ArrayList<IScatterDataSet>();
-        dataSets.add(dataSet);
-        ScatterData data = new ScatterData(dataSets);
-        scatterChart.setData(data);
+
+        ArrayList<Entry> entries = new ArrayList ();
+                entries.add(new Entry(0f, 0));
+                entries.add(new Entry(1f, 1));
+                entries.add(new Entry(2f, 2));
+//                entries.add(new Entry(12f, 3));
+//                entries.add(new Entry(18f, 4));
+//                entries.add(new Entry(9f, 5));
+
+        ScatterDataSet dataset = new ScatterDataSet(entries, "# per month");
+
+        ArrayList<String> months = new ArrayList<>();
+        months.add("Jan");
+        months.add("Feb");
+        months.add("Mar");
+
+        ArrayList<IScatterDataSet> dataSets = new ArrayList<>();
+        dataSets.add(dataset);
+        ScatterData d = new ScatterData(dataSets);
+        scatterChart.setData(d);
         scatterChart.invalidate();
+
+
+        // the labels that should be drawn on the XAxis
+        final String[] quarters = new String[] { "2018-11-26", "2018-11-28", "2018-11-27"};
+
+        IAxisValueFormatter formatter = new IAxisValueFormatter() {
+
+            @Override
+            public String getFormattedValue(float value, AxisBase axis) {
+                return quarters[(int) value];
+            }
+        };
+
+        XAxis xAxis = scatterChart.getXAxis();
+        xAxis.setGranularity(1f); // minimum axis-step (interval) is 1
+        xAxis.setValueFormatter(formatter);
+
+
+//        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+//        Date[] arrayOfDates = new Date[quarters.length];
+//        for (int index = 0; index < quarters.length; index++) {
+//            try {
+//                arrayOfDates[index] = sdf.parse(quarters[index]);
+//                Log.d("I", "To Date" +  quarters[index]);
+//            } catch (ParseException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//
+//        String [] results = new String[3];
+//
+//        Arrays.sort(arrayOfDates);
+//        for (int index = 0; index < results.length; index++) {
+//            results[index] = sdf.format(arrayOfDates[index]);
+//            Log.d("I", "From date" + results[index]);
+//        }
+
+
+//        //Testing Scatter Data: hard code point (10,10)
+//        ArrayList<Entry> list1 = new ArrayList<Entry>();
+//        //Populate the list with new "Entries". These will be data points of the
+//        list1.add(new Entry(10,10));
+//        ScatterDataSet dataSet = new ScatterDataSet(list1, "Kevin's Data");
+//        dataSet.setAxisDependency(YAxis.AxisDependency.LEFT);
+//        ArrayList<IScatterDataSet> dataSets = new ArrayList<IScatterDataSet>();
+//        dataSets.add(dataSet);
+//        ScatterData data = new ScatterData( dataSets);
+//        scatterChart.setData(data);
+//        scatterChart.invalidate();
     }
+
+
 
     public void chart(){
         int i = 0;
         ArrayList<Entry> completedDays = new ArrayList<Entry>();
+        ArrayList<String> dates = new ArrayList<>();
         //Populate the list with new "Entries". These will be data points of the
 //        int habitID = myDB.returnIDFromHT(habitName);
-        Cursor cursor = myDB.getAllDataHR(habitName);
+        Cursor cursor = myDB.getAllDataHRSortedByDate(habitName);
         if(cursor.getCount() == 0) {
             Toast.makeText(this, "No data to show", Toast.LENGTH_SHORT).show();
         }
         else {
             while(cursor.moveToNext()) {
                 int complete = Integer.valueOf(cursor.getString(2));
-//                int date = Integer.valueOf(cursor.getString(1));
+                String date = cursor.getString(1);
+                dates.add(date);
                 if(complete > 0) {
                     completedDays.add(new Entry(i ,1));
                 }else{
@@ -257,15 +346,63 @@ public class DescriptionActivity extends AppCompatActivity {
                 i++;
             }
         }
+
         ScatterDataSet dataSet = new ScatterDataSet(completedDays, "Kevin's Data");
         dataSet.setAxisDependency(YAxis.AxisDependency.LEFT);
+
+        int numberOfDates = dates.size();
+
+        String[] values = new String[numberOfDates];
+
+        for(int index = 0; i < numberOfDates-1; index++){
+            values[index] = dates.get(index);
+        }
+
+        Log.d("I", "Size of dates arraylist: " + dates.size());
+//        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+//        Date[] arrayOfDates = new Date[numberOfDates];
+//        for (int index = 0; index < numberOfDates; index++) {
+//            try {
+//                arrayOfDates[index] = sdf.parse(values[index]);
+//                Log.d("I", "To Date" +  values[index]);
+//            } catch (ParseException e) {
+//                e.printStackTrace();
+//            }
+//        }
+        //
+//        XAxis xAxis = scatterChart.getXAxis();
+//        xAxis.setValueFormatter(new MyXAxisValueFormatter(values));
+
         ArrayList<IScatterDataSet> dataSets = new ArrayList<IScatterDataSet>();
         dataSets.add(dataSet);
         ScatterData data = new ScatterData(dataSets);
         scatterChart.setData(data);
         scatterChart.invalidate();
+//
+//        IAxisValueFormatter formatter = new MyXAxisValueFormatter(values);
+//
+//        XAxis xAxis = scatterChart.getXAxis();
+//        xAxis.setGranularity(1f); // minimum axis-step (interval) is 1
+//        xAxis.setValueFormatter(formatter);
 
     }
+
+    public class MyXAxisValueFormatter implements IAxisValueFormatter {
+
+        private String[] mValues;
+
+        public MyXAxisValueFormatter(String[] values) {
+            this.mValues = values;
+        }
+
+        @Override
+        public String getFormattedValue(float value, AxisBase axis) {
+            // "value" represents the position of the label on the axis (x or y)
+            return mValues[(int) value];
+        }
+
+    }
+
 
 
 }
